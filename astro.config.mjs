045e847bf76b59d144b09fd34607ccad6082798d
xiaoqi-lab <xiaoqi777.zhang@gmail.com>
2025-12-29 16:24:41 +0800

    feat: change theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  compressHTML: true,
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
 });
