70553b24fd0f15035892d2424a42b08bc243145a
xiaoqi-lab <xiaoqi777.zhang@gmail.com>
2026-01-04 15:08:46 +0800

    chore: stop tracking node_modules and .astro

--- .astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_ZCroyp from "./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2Fcover-image%2Findex.md";
-import __ASTRO_IMAGE_IMPORT_Z38Rwq from "./logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2Fmarkdown-elements%2Findex.md";
-export default new Map([["./cover.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2Fcover-image%2Findex.md", __ASTRO_IMAGE_IMPORT_ZCroyp], ["./logo.png?astroContentImageFlag=&importer=src%2Fcontent%2Fpost%2Fmarkdown-elements%2Findex.md", __ASTRO_IMAGE_IMPORT_Z38Rwq]]);
-		
