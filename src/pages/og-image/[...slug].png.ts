f2e6a90c6bf998583fa2786e30a4f2a50de867f1
xiaoqi-lab <xiaoqi777.zhang@gmail.com>
2026-01-04 17:04:11 +0800

    feat: update

--- src/pages/og-image/[...slug].png.ts
+++ src/pages/og-image/[...slug].png.ts
@@ -1,5 +1,7 @@
 import { Resvg } from "@resvg/resvg-js";
 import type { APIContext, InferGetStaticPropsType } from "astro";
+import fs from "node:fs";
+import path from "node:path";
 import satori, { type SatoriOptions } from "satori";
 import { html } from "satori-html";
 import RobotoMonoBold from "@/assets/roboto-mono-700.ttf";
@@ -28,6 +30,11 @@ const ogOptions: SatoriOptions = {
 	width: 1200,
 };
 
+// 读取并转换 logo 图片
+const logoPath = path.resolve("public/logo.jpg");
+const logoBuffer = fs.readFileSync(logoPath);
+const logoBase64 = `data:image/jpeg;base64,${logoBuffer.toString("base64")}`;
+
 const markup = (title: string, pubDate: string) =>
 	html`<div tw="flex flex-col w-full h-full bg-[#1d1f21] text-[#c9cacc]">
 		<div tw="flex flex-col flex-1 w-full p-10 justify-center">
@@ -36,20 +43,7 @@ const markup = (title: string, pubDate: string) =>
 		</div>
 		<div tw="flex items-center justify-between w-full p-10 border-t border-[#2bbc89] text-xl">
 			<div tw="flex items-center">
-				<svg height="60" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 272 480">
-					<path
-						fill="#cdffb8"
-						d="M181.334 93.333v-40L226.667 80v40zM136.001 53.333 90.667 26.667v426.666L136.001 480zM45.333 220 0 193.334v140L45.333 360z"
-					/>
-					<path
-						fill="#d482ab"
-						d="M90.667 26.667 136.001 0l45.333 26.667-45.333 26.666zM181.334 53.33l45.333-26.72L272 53.33 226.667 80zM136 240l-45.333-26.67v53.34zM0 193.33l45.333-26.72 45.334 26.72L45.333 220zM181.334 93.277 226.667 120l-45.333 26.67z"
-					/>
-					<path
-						fill="#2abc89"
-						d="m136 53.333 45.333-26.666v120L226.667 120V80L272 53.333V160l-90.667 53.333v240L136 480V306.667L45.334 360V220l45.333-26.667v73.334L136 240z"
-					/>
-				</svg>
+				<img src="${logoBase64}" width="60" height="60" tw="rounded-full" />
 				<p tw="ml-3 font-semibold">${siteConfig.title}</p>
 			</div>
 			<p>by ${siteConfig.author}</p>
--- src/site.config.ts
+++ src/site.config.ts
@@ -48,6 +48,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		path: "/notes/",
 		title: "Notes",
 	},
+	{
+		path: "/tags/",
+		title: "Tags",
+	},
 ];
 
 // https://expressive-code.com/reference/configuration/
